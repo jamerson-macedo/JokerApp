29a0e3b7f10279443907dfc8e62f1be2e1df2749
jamerson Macedo <jamerson_macedo@hotmail.com>
2023-10-03 20:06:00 -0300

    Configurando Fragments e Navigation jetpack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.jokerapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.jokerapp"
@@ -36,11 +36,18 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.fragment:fragment:1.6.1")
+    val nav_version = "2.7.3"
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    val fragment_version = "1.6.1"
+
+    // Kotlin
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
